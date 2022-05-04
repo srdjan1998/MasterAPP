2cfcfcb9722c4ac29f74a5fb3dfa8e5c5a5a29df
srdjan1998 <starcevic.srdjan98@gmail.com>
2022-05-04 16:59:28 +0200

    Create-NodeJS-Server

---
+++ backend/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts' ,
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+
+        {
+            name: 'Adidas Fit shirt',
+            slug: 'adidas-fit-shirt',
+            category: 'Shirts' ,
+            image: '/images/p2.jpg',
+            price: 250,
+            countInStock: 20,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            category: 'Pants' ,
+            image: '/images/p3.jpg',
+            price: 50,
+            countInStock: 20,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality product',
+        },
+
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category: 'Pants' ,
+            image: '/images/p4.jpg',
+            price: 70,
+            countInStock: 15,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 9,
+            description: 'high quality product',
+        },
+
+    ],
+};
+
+export default data;
